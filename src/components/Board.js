8679eda802111062cb1644f6a50c0b1a5f7fa3dd
Deepteaman <diptimaan1234@gmail.com>
2021-05-04 04:56:04 +0530

    first commit

---
+++ src/components/Board.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Square from "./Square"
+
+export default class Board extends React.Component
+{
+    renderSquare = (i) => {
+        return(
+          <Square value = {this.props.squares[i]} handleClick = {() => this.props.clickAction(i)}/>
+        )
+      }
+    render(){return(
+        <div>
+            <div className="board-row">
+            {this.renderSquare(0)}          
+            {this.renderSquare(1)}          
+            {this.renderSquare(2)} 
+
+            </div>
+            <div className="board-row">
+            {this.renderSquare(4)}          
+            {this.renderSquare(5)}          
+            {this.renderSquare(6)} 
+
+            </div>
+            <div className="board-row">
+            {this.renderSquare(7)}          
+            {this.renderSquare(8)}          
+            {this.renderSquare(9)} 
+
+            </div>
+        </div>
+    ) 
+
+    
+}
+}
